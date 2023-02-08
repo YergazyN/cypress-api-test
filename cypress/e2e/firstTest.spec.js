4e5f5c09015b3a95937f69600d745d159d6a3b81
Yergazy <nurmaganbetov0486@gmail.com>
2023-02-08 11:46:59 +0600

    add article test

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -6,8 +6,22 @@ describe('Test with backend', ()=> {
         cy.loginToApplication()
     })
 
-    it('should log in', () => {
-        cy.log('We logged in!') 
-    })
+    it.only('verify correct request and response', ()=> {
+
+        cy.intercept('POST', 'https://api.realworld.io/api/articles/').as('postArticles')
+
+        cy.contains('New Article').click()
+        cy.get('[formcontrolname="title"]').type('Article One7')
+        cy.get('[formcontrolname="description"]').type('Article One Description')
+        cy.get('[formcontrolname="body"]').type('Article One Body')
+        cy.contains(' Publish Article ').click()
+        
+        cy.wait('@postArticles').then( xhr => {
+            console.log(xhr)
+            expect(xhr.response.statusCode).to.equal(200)
+            expect(xhr.request.body.article.body).to.equal('Article One Body')
+            expect(xhr.request.body.article.description).to.equal('Article One Description') 
+        })
+    });
 })
 
