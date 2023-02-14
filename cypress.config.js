b5ef092bc5bb717ce30859e64cd0905d0676ffc2
Yergazy <nurmaganbetov0486@gmail.com>
2023-02-14 10:28:41 +0600

    test retries

--- cypress.config.js
+++ cypress.config.js
@@ -5,21 +5,25 @@ module.exports = defineConfig({
   viewportWidth: 1920,
   env: {
     username: "nurmaganbetov0486@gmail.com",
-    password: "",
+    password: "Sabitzer15",
     apiUrl: "https://api.realworld.io"
   },
+  retries: {
+    runMode: 2,
+    // openMode: 0
+  },
   e2e: {
-    setupNodeEvents(on, config) {
-      const username = process.env.DB_USERNAME
-      const password = process.env.PASSWORD
+    // setupNodeEvents(on, config) {
+    //   const username = process.env.DB_USERNAME
+    //   const password = process.env.PASSWORD
 
-      if(!password) {
-        throw new Error(`missing PASSWORD environment variable`)
-      }
+    //   if(!password) {
+    //     throw new Error(`missing PASSWORD environment variable`)
+    //   }
 
-      config.env = {username, password}
-      return config
-    },
+    //   config.env = {username, password}
+    //   return config
+    // },
     baseUrl: 'http://localhost:4200/',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
     excludeSpecPattern: ['**/1-getting-started/*', '**/2-advanced-examples/*']
--- cypress/e2e/secondTest.spec.js
+++ cypress/e2e/secondTest.spec.js
@@ -4,7 +4,7 @@ describe('Test log out', () => {
         cy.loginToApplication()
     });
 
-    it('verify we can log out successfully', () => {
+    it('verify we can log out successfully', {retries: 2}, () => {
         cy.contains('Settings').click()
         cy.contains('Or click here to logout').click()
         cy.get('.navbar-nav').should('contain', 'Sign up')
