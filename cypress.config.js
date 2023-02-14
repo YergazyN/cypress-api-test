1e7f639d1e574c13d16cb9e240f6911ce0b33ff4
Yergazy <nurmaganbetov0486@gmail.com>
2023-02-14 14:37:31 +0600

    awsome reports

--- cypress.config.js
+++ cypress.config.js
@@ -12,18 +12,11 @@ module.exports = defineConfig({
     runMode: 2,
     // openMode: 0
   },
-  e2e: {
-    // setupNodeEvents(on, config) {
-    //   const username = process.env.DB_USERNAME
-    //   const password = process.env.PASSWORD
-
-    //   if(!password) {
-    //     throw new Error(`missing PASSWORD environment variable`)
-    //   }
-
-    //   config.env = {username, password}
-    //   return config
-    // },
+  reporter: 'cypress-multi-reporters',
+    reporterOptions: {
+      configFile: 'reporter-config.json',
+    },
+  e2e: {    
     baseUrl: 'http://localhost:4200/',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
     excludeSpecPattern: ['**/1-getting-started/*', '**/2-advanced-examples/*']
